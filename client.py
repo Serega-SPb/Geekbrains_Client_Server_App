dd94d8cba422ecb35cc27d70d71e47938f0fd7b0
Serega <abissovs@gmail.com>
2019-09-14 22:06:43 +0300

    fix client: - set default argument address = localhost - added functions <connect to server> and <presence> in wrapper

--- client.py
+++ client.py
@@ -12,21 +12,30 @@ class Client:
 
     TCP = (AF_INET, SOCK_STREAM)
     USER = f'Test{random.randint(0,1000)}'
+    connected = False
 
     def __init__(self, addr, port):
         self.socket = socket(*self.TCP)
         print_log(f'Connect to {addr}:{port}')
+        wrapper(self.start, addr, port)
+
+    def start(self, addr, port):
         self.socket.connect((addr, port))
+        self.connected = True
         self.presence()
 
     def __del__(self):
         self.send_request(Request(RequestAction.QUIT, self.USER))
 
     def send_request(self, request):
+        if not self.connected:
+            return
         print(request)
         wrapper(send_request, self.socket, request)
 
     def get_response(self):
+        if not self.connected:
+            return
         response = wrapper(get_data, self.socket)
         if response is not None:
             response = Response.from_dict(response)
@@ -46,17 +55,13 @@ class Client:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('addr', type=str, help='Server address')
+    parser.add_argument('addr', default='localhost', type=str, nargs='?', help='Server address')
     parser.add_argument('port', default=7777, type=int, nargs='?', help='Server port [default=7777]')
 
     args = parser.parse_args()
 
     addr = args.addr
     port = args.port
-
-    if addr is None:
-        print_error('Incorrect server address')
-        return
 
     client = Client(addr, port)
     client.send_msg('Hello server')
