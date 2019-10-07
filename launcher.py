417e06133a332631abd5dd0893104dde43e0523c
Serega <abissovs@gmail.com>
2019-10-07 13:58:55 +0300

    added readme launcher.py - added set port

---
+++ launcher.py
@@ -0,0 +1,38 @@
+import subprocess
+
+
+def add_subprocess(processes, cmd):
+    processes.append(subprocess.Popen(cmd, creationflags=subprocess.CREATE_NEW_CONSOLE))
+
+
+def main():
+    processes = []
+    port = 7878
+    exit_flag = False
+
+    while not exit_flag:
+
+        action = input('Select action:\n '
+                       'S - start server\n '
+                       'C - start client\n '
+                       'P - set port (default=7878)\n '
+                       'Q - all stop and quit\n').upper()
+
+        if action == 'S':
+            add_subprocess(processes, f'python server.py -p {port}')
+        elif action == 'C':
+            add_subprocess(processes, f'python client.py 127.0.0.1 {port}')
+        elif action == 'P':
+            p = input('Enter port: ')
+            if not p.isdigit() or not (1024 <= int(p) <= 65535):
+                print('Incorrect input')
+            else:
+                port = p
+        elif action == 'Q':
+            while processes:
+                processes.pop().kill()
+            exit(0)
+
+
+if __name__ == '__main__':
+    main()
